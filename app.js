630d8af8653697a2727094c56b4197202a75fc35
Ashish Meher <ashishmeher216@gmail.com>
2023-10-24 04:03:45 +0530

    upload to s3

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const { spawn } = require('child_process');
-const path = require('path');
 const cron = require('node-cron');
+const AWS = require('aws-sdk')
+const AdmZip = require('adm-zip')
 const env = require('dotenv');
-
 env.config({ path: __dirname + '/.env' });
+
 /* 
 Basic mongo dump and restore commands, they contain more options you can have a look at man page for both of them.
 1. mongodump --db=db_name --archive=./db_namec.gzip --gzip
@@ -21,18 +22,35 @@ Using mongorestore - without any args:
 // Note: 2nd expression only contains 5 fields, since seconds is not necessary
 
 // Scheduling the backup everyday at midnight (using node-cron)
+cron.schedule('*/60 * * * * *', () => backupMongoDB());  //every 60 seconds
+// cron.schedule('*/2 * * * *', () => backupMongoDB());  //every 2 minutes
 // cron.schedule('0 0 * * *', () => backupMongoDB());    //every midnight
-// cron.schedule('*/60 * * * * *', () => backupMongoDB());  //every 60 seconds
-cron.schedule('*/2 * * * *', () => backupMongoDB());  //every 2 minutes
 
+const MONGO_URL = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`;
+const mongoUrl = MONGO_URL;
+const bucketName = process.env.S3_BUCKET;
+const s3bucket = new AWS.S3({
+  params: {
+    accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
+    Bucket: bucketName
+  }
+});
+const s3StorageClass = process.env.S3_STORAGE_CLASS;
+const folderPrefix = process.env.FOLDER_PREFIX;
 
 function backupMongoDB() {
   const d = new Date();
-  const ARCHIVE_PATH = path.join(__dirname, '../../mongo-backups', `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`);
+  const fileName = `${d.getDate()}-${d.getMonth() + 1}-${d.getFullYear()}`;
+  
+  // const fileName = fileName;
+  const folderName = `/tmp/${fileName}/`;
+  let zipBuffer = null;
+
   const child = spawn('mongodump', [
     `--forceTableScan`,
-    `--uri=mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.ccvfl.mongodb.net/${process.env.MONGO_DB_DATABASE}`,
-    `--out=${ARCHIVE_PATH}`,
+    `--uri=${mongoUrl}`,
+    `--out=${folderName}`,
     // '--gzip',
   ]);
 
@@ -45,9 +63,29 @@ function backupMongoDB() {
   child.on('error', (error) => {
     console.log('error:\n', error);
   });
-  child.on('exit', (code, signal) => {
+  child.on('exit', async (code, signal) => {
     if (code) console.log('Process exit with code:', code);
     else if (signal) console.log('Process killed with signal:', signal);
-    else console.log('Backup is successfull ✅');
+    else {
+      try {
+        const zip = new AdmZip()
+        zip.addLocalFolder(folderName)
+        zipBuffer = zip.toBuffer()
+      } catch (err) {
+        throw new Error('archive creation failed: ', err)
+      }
+      try {
+        await s3bucket.upload({
+          Key: `${folderPrefix}/${fileName}.zip`,
+          Body: zipBuffer,
+          ContentType: 'application/zip',
+          ServerSideEncryption: 'AES256',
+          StorageClass: s3StorageClass
+        }).promise()
+      } catch (err) {
+        throw new Error('upload to S3 failed: ', err)
+      }
+      console.log('Backup is successfull ✅');
+    }
   });
 }
